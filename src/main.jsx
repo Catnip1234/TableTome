b4298afe3542a47415aa661a96a8a4823064dbe9
Catnip1234 <simon.daniel.picha@gmail.com>
2026-07-21 13:39:20 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,4 +2,4 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import App from './App.jsx'
-import './index.cs
+import './index.css
